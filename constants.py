0769becad6051258e22fa339f0b19f6af3c02e6a
ugtfo <90002772+ugtfo@users.noreply.github.com>
2023-05-07 11:32:49 +0300

    Delete constants.py

--- constants.py
+++
@@ -1,9 +0,0 @@
-WIDTH, HEIGHT = 800, 800
-
-FALSE = "-"
-
-COLOR_BG = ((255, 255, 255), 'white')
-COLOR_FILL_INIT = ((0.0, 0.0, 0.0), "#000000")
-COLOR_LINE = ((136, 0, 250), "#8800fa")
-
-FONT = "Verdana 12"
